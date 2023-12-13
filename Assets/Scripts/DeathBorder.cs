d853897fc77e8febdfbb720ffc1d8f04fdc26d7e
ajhalim <thenubbum@gmail.com>
2023-12-12 17:01:45 -0800

    particles?

--- Assets/Scripts/DeathBorder.cs
+++ Assets/Scripts/DeathBorder.cs
@@ -6,11 +6,13 @@ public class DeathBorder : MonoBehaviour
 {
 
     public int Lives = 3;
+    public ParticleSystem loseEffect;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag != "Chopsticks")
         {
+            GameObject.Instantiate(loseEffect, transform.position, transform.rotation);
             Destroy(other.gameObject);
             Lives--;
         }
